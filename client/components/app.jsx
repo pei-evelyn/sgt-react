3371712e8bdd06757c97f6774c4a26607b7cfd81
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-21 13:14:29 -0700

    Added header and grade-table files and fatch method to app

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 
+import Header from './header';
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grades: []
+    };
+  }
+
+  componentDidMount() {
+    this.getAllGrades();
+  }
+
+  getAllGrades() {
+    fetch('/api/grades')
+      .then(res => res.json())
+      .then(grades =>
+        this.setState(state => {
+          // console.log(grades);
+          return { grades: grades };
+        }))
+      .catch(err => console.error(err));
+  }
+
   render() {
-    return null;
+    return (
+      <Header />
+    );
   }
 }
 
