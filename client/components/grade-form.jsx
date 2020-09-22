4c93ad7882906267847738a79d42b892314b9a7d
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-21 17:43:23 -0700

    Edited form styling

--- client/components/grade-form.jsx
+++ client/components/grade-form.jsx
@@ -3,27 +3,33 @@ import React from 'react';
 function GradeForm(props) {
   return (
     <form className="col-lg-4">
-      <div className="input-group">
+      <div className="input-group form-group">
         <div className="input-group-prepend">
-          <i className="fas fa-user"></i>
+          <div className="input-group-text px-3">
+            <i className="fas fa-user"></i>
+          </div>
         </div>
         <input type="text" className="form-control" placeholder="Name"/>
       </div>
-      <div className="input-group">
+      <div className="input-group form-group">
         <div className="input-group-prepend">
-          <i className="fas fa-list-alt"></i>
+          <div className="input-group-text px-3">
+            <i className="fas fa-list-alt"></i>
+          </div>
         </div>
         <input type="text" className="form-control" placeholder="Course"/>
       </div>
-      <div className="input-group">
+      <div className="input-group form-group">
         <div className="input-group-prepend">
-          <i className="fas fa-graduation-cap"></i>
+          <div className="input-group-text px-3">
+            <i className="fas fa-graduation-cap"></i>
+          </div>
         </div>
-        <input type="text" className="form-control" placeholder="Course" />
+        <input type="text" className="form-control" placeholder="Grade"/>
       </div>
-      <div>
-        <button type="submit" className="btn btn-primary">Add</button>
-        <button type="reset" className="btn btn-outline-primary">Cancel</button>
+      <div className="d-flex justify-content-end">
+        <button type="submit" className="btn btn-info">Add</button>
+        <button type="reset" className="btn btn-outline-secondary ml-2">Cancel</button>
       </div>
     </form>
   );
